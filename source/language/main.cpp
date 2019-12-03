56c6178062be0685d9564495fc98bafdd8b91f16
daniel.rehman <daniel.rehman@wsu.edu>
2019-12-03 13:24:39 -0800

    add ds store.

--- source/language/main.cpp
+++ source/language/main.cpp
@@ -1,13 +1,20 @@
-#include "compiler.hpp"
-#include "interpreter.hpp"
+//#include "compiler.hpp"
+//#include "interpreter.hpp"
+
+#include <iostream>
 
 int main(const int argc, const char** argv) {
-    auto args = get_commandline_arguments(argc, argv);
-    initialize_llvm();
-    llvm::LLVMContext context;
-    if (args.use_repl) repl(context);
-    auto module = link(frontend(args, context));
-    optimize(module);
-    if (args.use_interpreter) interpret(module, args);
-    else emit_executable(generate_object_file(module, args), args);
+    
+    std::cout << "hello world!\n";
+    
+    
+//    auto args = get_commandline_arguments(argc, argv);
+//    initialize_llvm();
+//    llvm::LLVMContext context;
+//    if (args.use_repl) repl(context);
+//    auto module = link(frontend(args, context));
+//    optimize(module);
+//    if (args.use_interpreter) interpret(module, args);
+//    else emit_executable(generate_object_file(module, args), args);
 }
+
