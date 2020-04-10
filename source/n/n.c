5c27dab38a7bae6bafbb76095cd51130a09ec778
dwrrehman <dwrrehman@gmail.com>
2020-04-10 14:39:17 -0700

    start the process of massively changing the csr algorithm so signatures are just resolved exprs.

--- source/n/n.c
+++ source/n/n.c
@@ -15,7 +15,7 @@ struct expression {
 };
 
 struct resolved {
-    size_t id;
+    size_t index;
     size_t value;
     size_t count;
     struct resolved* arguments;
@@ -23,8 +23,8 @@ struct resolved {
 
 struct name {
     size_t type;
-    size_t length;
-    struct resolved* signature;
+    struct resolved signature;
+    size_t length; /// Do we need this?
 };
 
 struct frame {
@@ -71,44 +71,95 @@ static struct expression parse(const char* filename, const char* text, size_t* a
 }
 
 static void represent(size_t given, char* buffer, size_t limit, size_t* at, struct context* context) {
+    if (given >= context->name_count) return;
     struct name name = context->names[given];
-    for (size_t i = 0; i < name.length; i++) {
-        if (*at + 2 >= limit) return;
-        else if (name.signature[i].value) buffer[(*at)++] = name.signature[i].value;
-        else {
-            buffer[(*at)++] = '(';
-            represent(name.signature[i].id, buffer, limit, at, context);
-            buffer[(*at)++] = ')';
-        }
-    }
+//    for (size_t i = 0; i < name.length; i++) {
+//        if (*at + 2 >= limit) return;
+//        else if (name.signature[i].value) buffer[(*at)++] = name.signature[i].value;
+//        else {
+//            buffer[(*at)++] = '(';
+//            represent(name.signature[i].index, buffer, limit, at, context);
+//            buffer[(*at)++] = ')';
+//        }
+//    }
     if (name.type) {
         buffer[(*at)++] = ' ';
         represent(name.type, buffer, limit, at, context);
     }
 }
 
-static size_t not_equal(size_t a, size_t b, struct context* context) {
-    for (size_t i = 0; i < context->frames[context->frame_count - 1].count; i++) {
-        if (context->frames[context->frame_count - 1].indicies[i] == a) {
-            return a != b;
-        }
-    }
-    return 0;
-}
-
 static struct resolved resolve(struct expression, size_t, struct context*);
 
 static struct resolved resolve_at(struct expression given, size_t type, struct context* context, size_t depth) {
-    if (depth > 128) return (struct resolved) {0};    
+    if (depth > 128) return (struct resolved) {0};
     else if (context->at < given.count && !given.symbols[context->at].value) return resolve(given.symbols[context->at++], type, context);
-
+    
     size_t saved = context->at;
     struct frame top = context->frames[context->frame_count - 1];
     
     for (size_t i = 0; i < top.count; i++) {
         context->best = fmax(context->at, context->best); context->at = saved;
         struct resolved solution = {top.indicies[i], 0, 0, 0};
-        struct name name = context->names[solution.id];
+        struct name name = context->names[solution.index];
+        
+        if (name.type != type) continue;
+        
+        if (name.signature.index == 3) { // (c) sig
+            if (name.signature.value != given.symbols[context->at].value) continue;
+            context->at++;
+        }
+
+        if (name.signature.index == 4) { // Decl-param(sig)(type) sig
+            
+            ///IS THIS RIGHTTT?????!!?
+            if (context->at >= given.count) {
+                if (solution.count == 1 && s == 1) return solution.arguments[0]; else goto next;
+            } /// where do we put thiss!??!
+            
+            
+            /// call match_signature() on name.signature.arguments[0];
+            
+            struct resolved argument = resolve_at(given, name.signature.arguments[1].index, context, depth + 1);
+            
+            if (!argument.index) goto next;
+            
+            solution.arguments = realloc(solution.arguments, sizeof(struct resolved) * (solution.count + 1));
+            solution.arguments[solution.count++] = argument;
+            
+            return argument; ///????
+        }
+                
+        for (each argument to s) {
+            sol = resolve(given, type, context, depth);
+            if (!sol.index) return 0; /// continue to thenext signature.
+        }
+                
+        return solution;
+        
+        next: continue; /// we might not need a goto statement at all with this change!
+    }
+    /// I THINK HERE
+    
+    /// is where we should give errors. always.
+    /// we dont need a seperate resolve() function for printing errors anymore.
+    
+    
+    return (struct resolved) {0};
+}
+
+/*
+
+static struct resolved resolve_at(struct expression given, size_t type, struct context* context, size_t depth) {
+    if (depth > 128) return (struct resolved) {0};
+    else if (context->at < given.count && !given.symbols[context->at].value) return resolve(given.symbols[context->at++], type, context);
+    
+    size_t saved = context->at;
+    struct frame top = context->frames[context->frame_count - 1];
+    
+    for (size_t i = 0; i < top.count; i++) {
+        context->best = fmax(context->at, context->best); context->at = saved;
+        struct resolved solution = {top.indicies[i], 0, 0, 0};
+        struct name name = context->names[solution.index];
         
         if (name.type != type) continue;
         
@@ -117,8 +168,8 @@ static struct resolved resolve_at(struct expression given, size_t type, struct c
                 if (solution.count == 1 && s == 1) return solution.arguments[0]; else goto next;
             }
             if (!name.signature[s].value) {
-                struct resolved argument = resolve_at(given, context->names[name.signature[s].id].type, context, depth + 1);
-                if (!argument.id || not_equal(name.type, type, context)) goto next;
+                struct resolved argument = resolve_at(given, context->names[name.signature[s].index].type, context, depth + 1);
+                if (!argument.index) goto next;
                 solution.arguments = realloc(solution.arguments, sizeof(struct resolved) * (solution.count + 1));
                 solution.arguments[solution.count++] = argument;
             } else if (name.signature[s].value != given.symbols[context->at].value) goto next; else context->at++;
@@ -130,12 +181,14 @@ static struct resolved resolve_at(struct expression given, size_t type, struct c
     return (struct resolved) {0};
 }
 
+*/
+
 static struct resolved resolve(struct expression given, size_t type, struct context* context) {
     struct context sub = *context; sub.at = sub.best = 0;
     struct resolved solution = resolve_at(given, type, &sub, 0);
-    if (sub.at < given.count) solution.id = 0;
+    if (sub.at < given.count) solution.index = 0;
     
-    if (!solution.id) {
+    if (!solution.index) {
         struct expression b = sub.best < given.count ? given.symbols[sub.best] : given;
         char buffer[2048] = {0}; size_t index = 0;
         represent(type, buffer, sizeof buffer,  &index, context);
@@ -170,17 +223,13 @@ static void debug_context(struct context* context) {
         size_t index = 0;
         represent(i, buffer, sizeof buffer, &index, context);
         printf("%s", buffer);
-//        memset(buffer, 0, sizeof buffer);
-//        index = 0;
-//        represent(context->names[i].definition.id, buffer, sizeof buffer, &index, context);
-//        if (context->names[i].signature[1].id) printf("      %s", buffer);
         puts("\n");
     }
     puts("}");
 }
 
 static void debug_resolved(struct resolved given) {
-    printf(" [%lu]:%c:{ ", given.id, (char) given.value);
+    printf(" [%lu]:%c:{ ", given.index, (char) given.value);
     for (size_t i = 0; i < given.count; i++) {
         printf("ARG(%lu)[", i);
         debug_resolved(given.arguments[i]);
@@ -191,134 +240,37 @@ static void debug_resolved(struct resolved given) {
 
 int main(const int argc, const char** argv) {
     for (int i = 1; i < argc; i++) {
-        if (!strcmp("version", argv[i])) exit(!puts("n: 0.0.3"));
-        if (!strcmp("usage", argv[i])) exit(!puts("n version/usage [files]"));
-        
-//        char* text = open_file(argv[i]);
-//        if (!text) continue;
-//        size_t at = 0;
-//        uint32_t line = 1, column = 1;
-//        struct expression expression = parse(argv[i], text, &at, &line, &column);
-//        debug_expression(expression); puts("\n");
-        
+        if (!strcmp("version", argv[i])) exit(!puts("n3zqx2l: 0.0.3"));
+        if (!strcmp("usage", argv[i])) exit(!puts("n3zqx2l version/usage [files]"));
+                
+        char* text = open_file(argv[i]);
+        if (!text) continue;
+        size_t at = 0;
+        uint32_t line = 1, column = 1;
+        struct expression expression = parse(argv[i], text, &at, &line, &column);
+        debug_expression(expression); puts("\n");
+                        
         struct context context = {0};
         context.frames = calloc(context.frame_count = 1, sizeof(struct frame));
         context.filename = argv[i];
-        
-        printf("the CSR terminal. used for debugging purposes. type 'h' for help.\n");
-        while (1) {
-            
-            printf(": ");
-            int c = getchar();
-            
-            if (c == 'q') break;
-            
-            else if (c == 'h') {
-                printf("a : add new name\n"
-                       "s : print state\n"
-                       "u : push new frame index\n"
-                       "o : pop last frame index\n"
-                       "d : pop last name\n"
-                       "h : this help menu\n"
-                       "q : quit terminal\n"
-                       );
-                
-            } else if (c == 'a') {
-                
-                struct name n = {0};
-                printf("adding signature\n");
-                
-                printf("type: ");
-                scanf("%lu", &n.type);
-            
-                printf("give symbols.\n for a parameter, give (, then type a number, then type ).\n to terminate the signature, type ;\n");
-                while (c != ';') {
-                    
-                    printf("symbol: ");
-                    c = getchar();
-                    printf("received: %d\n", c);
-                    
-                    if (c == '(') {
-                        
-                        struct resolved param = {0};
-                        
-                        printf("index: ");
-                        scanf("%lu", &param.id);
-                        
-                        printf("close paren: ");
-                        getchar();
-                        
-                        n.signature = realloc(n.signature, sizeof(struct resolved) * (n.length + 1));
-                        n.signature[n.length++] = param;
-                        
-                    } else if (c != '\n' && c != ';') {
-                        n.signature = realloc(n.signature, sizeof(struct resolved) * (n.length + 1));
-                        n.signature[n.length++].value = c;
-                    }
-                }
-                printf("signature terminated. adding now.");
-                context.names = realloc(context.names, sizeof(struct name) * (context.name_count + 1));
-                context.names[context.name_count++] = n;
-                printf("added.\n");
-                
-            } else if (c == 's') {
-                
-                printf("printing context: \n\n");
-                debug_context(&context);
-                
-            } else if (c == 'd') {
-                printf("decrementing name count..\n");
-                context.name_count--;
-                
-            } else if (c == 'o') {
-                
-                printf("decrementing stack frame indicies count..\n");
-                context.frames[0].count--;
-                
-            } else if (c == 'u') {
-                
-                size_t integer = 0;
-                printf("index to push: ");
-                scanf("%lu", &integer);
-                
-                context.frames[0].indicies = realloc(context.frames[0].indicies, sizeof(size_t) * (context.frames[0].count + 1));
-                context.frames[0].indicies[context.frames[0].count++] = integer;
-            } else if (c == 'f') {
-                
-                printf("performing CSR:\n");
-                size_t expected_type = 0;
-                printf("expected type: ");
-                scanf("%lu", &expected_type);
-                if (expected_type >= context.name_count) {
-                    printf("error: expected type not in range! trying 0 instead.\n");
-                    expected_type = 0;
-                }
-                
-                printf("reading in file again...\n");
-                char* text = open_file(argv[i]);
-                if (!text) continue;
-                size_t at = 0;
-                uint32_t line = 1, column = 1;
-                struct expression expression = parse(argv[i], text, &at, &line, &column);
-                debug_expression(expression); puts("\n");
-                
-                
-                context.at = context.best = 0;
-                struct resolved resolved = resolve(expression, expected_type, &context);
-                debug_resolved(resolved);
-                
-                puts("\n");
+        context.at = context.best = 0;
 
-                if (!resolved.id) {
-                    printf("\nRESOLUTION ERROR\n");
-                }
-                                
-            } else if (c == '\n') {
-                // do nothing.
-            } else {
-                printf("ERROR: unrecognized command: %c\n", c);
-            }
-        }
-        printf("terminated terminal.\n");
+        
+        // n.signature = realloc(n.signature, sizeof(struct resolved) * (n.length + 1));
+        
+        // n.signature = realloc(n.signature, sizeof(struct resolved) * (n.length + 1));
+        // n.signature[n.length++].value = c;
+        
+         context.names = realloc(context.names, sizeof(struct name) * (context.name_count + 1));
+         context.names[context.name_count++] = (struct name){0};
+
+        
+         context.frames[0].indicies = realloc(context.frames[0].indicies, sizeof(size_t) * (context.frames[0].count + 1));
+         context.frames[0].indicies[context.frames[0].count++] = 0;
+        
+        debug_context(&context);
+        struct resolved resolved = resolve(expression, 0, &context);
+        debug_resolved(resolved);
+        if (!resolved.index) printf("\nRESOLUTION ERROR\n");
     }
 }
