7e704a079be3fa15ca67879195049a8204141718
dwrrehman <dwrrehman@gmail.com>
2020-04-10 17:45:16 -0700

    i did it. i made parenthesis not special in the language anymore. no more parenthesis. they are just like any other character now. we are offically more minimalist than lisp.

--- source/n/n.c
+++ source/n/n.c
@@ -6,31 +6,28 @@
 #include <string.h>
 #include <ctype.h>
 
-struct expression {
+struct token {
     size_t value;
-    size_t count;
-    struct expression* symbols;
     uint32_t line;
     uint32_t column;
-};
+}; // 8 + 4 + 4 = 16
 
 struct resolved {
     size_t index;
     size_t value;
     size_t count;
     struct resolved* arguments;
-};
+}; // 8 + 8 + 8 + 8 = 64
 
 struct name {
     size_t type;
     struct resolved signature;
-    size_t length; /// Do we need this?
-};
+}; // 8 + 64 = 72
 
 struct frame {
     size_t count;
     size_t* indicies;
-};
+}; // 8 + 8 = 16
 
 struct context {
     size_t at;
@@ -43,56 +40,54 @@ struct context {
     const char* filename;
 };
 
-static char* open_file(const char* filename) {
+static struct token* lex_file(const char* filename, size_t* count) {
     FILE* file = fopen(filename, "r");
     if (!file) { fprintf(stderr, "n: %s: ", filename); perror("error"); return NULL; }
     fseek(file, 0, SEEK_END);
-    const size_t length = ftell(file);
-    char* buffer = (char*) calloc(length + 1, sizeof(char));
+    size_t length = ftell(file);
+    char* text = (char*) calloc(length + 1, sizeof(char));
     fseek(file, 0, SEEK_SET);
-    fread(buffer, sizeof(char), length, file);
+    fread(text, sizeof(char), length, file);
     fclose(file);
-    return buffer;
-}
-
-static struct expression parse(const char* filename, const char* text, size_t* at, uint32_t* line, uint32_t* column) {
-    struct expression result = {0};
-    while (text[*at] && text[*at] != ')') {
-        if (text[*at] == '\n') { ++*line; *column = 1;} else ++*column;
-        if (isspace(text[(*at)++])) continue;
-        uint32_t l = *line, c = *column - 1;
-        result.symbols = realloc(result.symbols, sizeof(struct expression) * (result.count + 1));
-        if (text[*at - 1] == '(') {
-            result.symbols[result.count] = parse(filename, text, at, line, column);
-            result.symbols[result.count].line = l; result.symbols[result.count++].column = c;
-            if (!text[*at] || text[(*at)++] != ')') fprintf(stderr, "n3zqx2l: %s:%u:%u: error: expected )\n\n", filename, l, c); ++*column;
-        } else result.symbols[result.count++] = (struct expression){text[*at - 1], 0, 0, l, c};
-    } return result;
+    
+    struct token* tokens = malloc(length * sizeof(struct token));
+    uint32_t line = 1, column = 1;
+    while (*text) {
+        if (!isspace(*text)) tokens[(*count)++] = (struct token){*text, line, column};
+        if (*text++ == '\n') { line++; column = 1;} else column++;
+    }
+    return realloc(tokens, sizeof(struct token) * *count);
 }
 
 static void represent(size_t given, char* buffer, size_t limit, size_t* at, struct context* context) {
+    
+    ///Unfinsihed.
+    
     if (given >= context->name_count) return;
+    if (*at >= limit) return;
     struct name name = context->names[given];
-//    for (size_t i = 0; i < name.length; i++) {
-//        if (*at + 2 >= limit) return;
-//        else if (name.signature[i].value) buffer[(*at)++] = name.signature[i].value;
-//        else {
-//            buffer[(*at)++] = '(';
-//            represent(name.signature[i].index, buffer, limit, at, context);
-//            buffer[(*at)++] = ')';
-//        }
-//    }
+    
+    if (name.signature.value) buffer[(*at)++] = name.signature.value; // appchar.
+    else if(name.signature.index == 4) { // decl
+        buffer[(*at)++] = '(';
+        represent(name.signature.index, buffer, limit, at, context);
+        buffer[(*at)++] = ')';
+    } else {
+        for (size_t i = 0; i < name.signature.count; i++) {
+            represent(name.signature.arguments[i].index, buffer, limit, at, context);
+        }
+    }
+    
     if (name.type) {
         buffer[(*at)++] = ' ';
         represent(name.type, buffer, limit, at, context);
     }
 }
 
-static struct resolved resolve(struct expression, size_t, struct context*);
+static struct resolved resolve(struct token*, size_t, size_t, struct context*);
 
-static struct resolved resolve_at(struct expression given, size_t type, struct context* context, size_t depth) {
+static struct resolved resolve_at(struct token* given, size_t given_count, size_t type, struct context* context, size_t depth) {
     if (depth > 128) return (struct resolved) {0};
-    else if (context->at < given.count && !given.symbols[context->at].value) return resolve(given.symbols[context->at++], type, context);
     
     size_t saved = context->at;
     struct frame top = context->frames[context->frame_count - 1];
@@ -105,21 +100,21 @@ static struct resolved resolve_at(struct expression given, size_t type, struct c
         if (name.type != type) continue;
         
         if (name.signature.index == 3) { // (c) sig
-            if (name.signature.value != given.symbols[context->at].value) continue;
+            if (name.signature.value != given[context->at].value) continue;
             context->at++;
         }
 
         if (name.signature.index == 4) { // Decl-param(sig)(type) sig
             
             ///IS THIS RIGHTTT?????!!?
-            if (context->at >= given.count) {
-                if (solution.count == 1 && s == 1) return solution.arguments[0]; else goto next;
+            if (context->at >= given_count) {
+//                if (solution.count == 1 && s == 1) return solution.arguments[0]; else goto next;
             } /// where do we put thiss!??!
             
             
             /// call match_signature() on name.signature.arguments[0];
             
-            struct resolved argument = resolve_at(given, name.signature.arguments[1].index, context, depth + 1);
+            struct resolved argument = resolve_at(given, given_count, name.signature.arguments[1].index, context, depth + 1);
             
             if (!argument.index) goto next;
             
@@ -129,10 +124,10 @@ static struct resolved resolve_at(struct expression given, size_t type, struct c
             return argument; ///????
         }
                 
-        for (each argument to s) {
-            sol = resolve(given, type, context, depth);
-            if (!sol.index) return 0; /// continue to thenext signature.
-        }
+//        for (each argument to s) {
+//            sol = resolve(given, type, context, depth);
+//            if (!sol.index) return 0; /// continue to thenext signature.
+//        }
                 
         return solution;
         
@@ -142,7 +137,6 @@ static struct resolved resolve_at(struct expression given, size_t type, struct c
     
     /// is where we should give errors. always.
     /// we dont need a seperate resolve() function for printing errors anymore.
-    
     
     return (struct resolved) {0};
 }
@@ -183,28 +177,18 @@ static struct resolved resolve_at(struct expression given, size_t type, struct c
 
 */
 
-static struct resolved resolve(struct expression given, size_t type, struct context* context) {
+static struct resolved resolve(struct token* given, size_t given_count, size_t type, struct context* context) {
     struct context sub = *context; sub.at = sub.best = 0;
-    struct resolved solution = resolve_at(given, type, &sub, 0);
-    if (sub.at < given.count) solution.index = 0;
+    struct resolved solution = resolve_at(given, given_count, type, &sub, 0);
+    if (sub.at < given_count) solution.index = 0;
     
     if (!solution.index) {
-        struct expression b = sub.best < given.count ? given.symbols[sub.best] : given;
+        struct token b = sub.best < given_count ? given[sub.best] : given[0];
         char buffer[2048] = {0}; size_t index = 0;
         represent(type, buffer, sizeof buffer,  &index, context);
         printf("n3zqx2l: %s:%u:%u: error: %s: unresolved %s%c\n\n", context->filename, b.line, b.column, buffer, b.value ? "symbol " : "expression", (char) b.value);
     }
     return solution;
-}
-
-
-static void debug_expression(struct expression e) {
-    if (e.value) printf(" %c[%u,%u] ", (char) e.value, e.line, e.column);
-    else {
-        printf(" ( ");
-        for (size_t i = 0; i < e.count; i++) debug_expression(e.symbols[i]);
-        printf(" ) [%u,%u] ", e.line, e.column);
-    }
 }
 
 static void debug_context(struct context* context) {
