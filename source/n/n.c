56de48b1100af97ba9e6ea494c5aece0fa6bd860
dwrrehman <dwrrehman@gmail.com>
2020-04-10 18:10:35 -0700

    parenthesis being special is now a faint memory, and the compiler became radically simpler as a result. there now is only one stage of the compiler. resolve. no parsing. lexing is done when we open the file.

--- source/n/n.c
+++ source/n/n.c
@@ -10,54 +10,33 @@ struct token {
     size_t value;
     uint32_t line;
     uint32_t column;
-}; // 8 + 4 + 4 = 16
+};
 
 struct resolved {
     size_t index;
     size_t value;
     size_t count;
     struct resolved* arguments;
-}; // 8 + 8 + 8 + 8 = 64
+};
 
 struct name {
-    size_t type;
     struct resolved signature;
-}; // 8 + 64 = 72
+    size_t type;
+};
 
 struct frame {
     size_t count;
     size_t* indicies;
-}; // 8 + 8 = 16
+};
 
 struct context {
     size_t at;
     size_t best;
     size_t frame_count;
     size_t name_count;
-    size_t UNUSED;
     struct frame* frames;
     struct name* names;
-    const char* filename;
 };
-
-static struct token* lex_file(const char* filename, size_t* count) {
-    FILE* file = fopen(filename, "r");
-    if (!file) { fprintf(stderr, "n: %s: ", filename); perror("error"); return NULL; }
-    fseek(file, 0, SEEK_END);
-    size_t length = ftell(file);
-    char* text = (char*) calloc(length + 1, sizeof(char));
-    fseek(file, 0, SEEK_SET);
-    fread(text, sizeof(char), length, file);
-    fclose(file);
-    
-    struct token* tokens = malloc(length * sizeof(struct token));
-    uint32_t line = 1, column = 1;
-    while (*text) {
-        if (!isspace(*text)) tokens[(*count)++] = (struct token){*text, line, column};
-        if (*text++ == '\n') { line++; column = 1;} else column++;
-    }
-    return realloc(tokens, sizeof(struct token) * *count);
-}
 
 static void represent(size_t given, char* buffer, size_t limit, size_t* at, struct context* context) {
     
@@ -83,8 +62,6 @@ static void represent(size_t given, char* buffer, size_t limit, size_t* at, stru
         represent(name.type, buffer, limit, at, context);
     }
 }
-
-static struct resolved resolve(struct token*, size_t, size_t, struct context*);
 
 static struct resolved resolve_at(struct token* given, size_t given_count, size_t type, struct context* context, size_t depth) {
     if (depth > 128) return (struct resolved) {0};
@@ -141,58 +118,24 @@ static struct resolved resolve_at(struct token* given, size_t given_count, size_
     return (struct resolved) {0};
 }
 
-/*
 
-static struct resolved resolve_at(struct expression given, size_t type, struct context* context, size_t depth) {
-    if (depth > 128) return (struct resolved) {0};
-    else if (context->at < given.count && !given.symbols[context->at].value) return resolve(given.symbols[context->at++], type, context);
-    
-    size_t saved = context->at;
-    struct frame top = context->frames[context->frame_count - 1];
-    
-    for (size_t i = 0; i < top.count; i++) {
-        context->best = fmax(context->at, context->best); context->at = saved;
-        struct resolved solution = {top.indicies[i], 0, 0, 0};
-        struct name name = context->names[solution.index];
-        
-        if (name.type != type) continue;
-        
-        for (size_t s = 0; s < name.length; s++) {
-            if (context->at >= given.count) {
-                if (solution.count == 1 && s == 1) return solution.arguments[0]; else goto next;
-            }
-            if (!name.signature[s].value) {
-                struct resolved argument = resolve_at(given, context->names[name.signature[s].index].type, context, depth + 1);
-                if (!argument.index) goto next;
-                solution.arguments = realloc(solution.arguments, sizeof(struct resolved) * (solution.count + 1));
-                solution.arguments[solution.count++] = argument;
-            } else if (name.signature[s].value != given.symbols[context->at].value) goto next; else context->at++;
-        }
-        
-        return solution;
-        next: continue;
-    }
-    return (struct resolved) {0};
-}
-
-*/
-
-static struct resolved resolve(struct token* given, size_t given_count, size_t type, struct context* context) {
+/// Scheduled for Deletion, and absorbtion into the resolve_at() function.
+static struct resolved resolve(struct token* given, size_t given_count, size_t type, struct context* context, const char* filename) {
     struct context sub = *context; sub.at = sub.best = 0;
     struct resolved solution = resolve_at(given, given_count, type, &sub, 0);
     if (sub.at < given_count) solution.index = 0;
     
     if (!solution.index) {
-        struct token b = sub.best < given_count ? given[sub.best] : given[0];
+        if (sub.best >= given_count) abort();
+        struct token b = given[sub.best];
         char buffer[2048] = {0}; size_t index = 0;
         represent(type, buffer, sizeof buffer,  &index, context);
-        printf("n3zqx2l: %s:%u:%u: error: %s: unresolved %s%c\n\n", context->filename, b.line, b.column, buffer, b.value ? "symbol " : "expression", (char) b.value);
+        printf("n3zqx2l: %s:%u:%u: error: %s: unresolved %s%c\n\n", filename, b.line, b.column, buffer, b.value ? "symbol " : "expression", (char) b.value);
     }
     return solution;
 }
 
 static void debug_context(struct context* context) {
-    printf("filename = %s\n", context->filename);
     printf("index = %lu, best = %lu\n", context->at, context->best);
     printf("---- debugging frames: ----\n");
     for (size_t i = 0; i < context->frame_count; i++) {
@@ -222,39 +165,43 @@ static void debug_resolved(struct resolved given) {
     printf(" } ");
 }
 
-int main(const int argc, const char** argv) {
+int main(int argc, const char** argv) {
     for (int i = 1; i < argc; i++) {
         if (!strcmp("version", argv[i])) exit(!puts("n3zqx2l: 0.0.3"));
         if (!strcmp("usage", argv[i])) exit(!puts("n3zqx2l version/usage [files]"));
-                
-        char* text = open_file(argv[i]);
-        if (!text) continue;
-        size_t at = 0;
+        
+        FILE* file = fopen(argv[i], "r");
+        if (!file) {
+            fprintf(stderr, "n: %s: ", argv[i]);
+            perror("error");
+            continue;
+        }
+        fseek(file, 0, SEEK_END);
+        size_t length = ftell(file);
+        char* text = malloc((length) * sizeof(char));
+        fseek(file, 0, SEEK_SET);
+        fread(text, sizeof(char), length, file);
+        fclose(file);
+        
+        size_t count = 0;
         uint32_t line = 1, column = 1;
-        struct expression expression = parse(argv[i], text, &at, &line, &column);
-        debug_expression(expression); puts("\n");
-                        
+        struct token* tokens = malloc(length * sizeof(struct token));
+        for (size_t i = 0; i < length; i++) {
+            if (!isspace(text[i])) tokens[count++] = (struct token){text[i], line, column};
+            if (text[i] == '\n') { line++; column = 1;} else column++;
+        }
+        
         struct context context = {0};
         context.frames = calloc(context.frame_count = 1, sizeof(struct frame));
-        context.filename = argv[i];
-        context.at = context.best = 0;
-
         
-        // n.signature = realloc(n.signature, sizeof(struct resolved) * (n.length + 1));
-        
-        // n.signature = realloc(n.signature, sizeof(struct resolved) * (n.length + 1));
-        // n.signature[n.length++].value = c;
-        
-         context.names = realloc(context.names, sizeof(struct name) * (context.name_count + 1));
-         context.names[context.name_count++] = (struct name){0};
-
-        
-         context.frames[0].indicies = realloc(context.frames[0].indicies, sizeof(size_t) * (context.frames[0].count + 1));
-         context.frames[0].indicies[context.frames[0].count++] = 0;
-        
+        struct resolved resolved = resolve(tokens, count, 0, &context, argv[i]);
         debug_context(&context);
-        struct resolved resolved = resolve(expression, 0, &context);
         debug_resolved(resolved);
         if (!resolved.index) printf("\nRESOLUTION ERROR\n");
+                
+        free(context.frames);
+        free(context.names);
+        free(tokens);
+        free(text);
     }
 }
