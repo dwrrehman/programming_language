6d1871ae58a35a96e9e65b69885483f81bbd5314
dwrrehman <dwrrehman@gmail.com>
2020-05-13 12:00:11 -0700

    add some more whitespace back in, so its only 158 lines now.

--- source/n/n.c
+++ source/n/n.c
@@ -5,8 +5,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-enum { _U = 256, _i, _n, _s, _c, _a0, _a, _p0, _p, _d0, _d1, _d, _k0, _k1, _k, _j0, _j1, _j, _intrin_count };
-enum { _cg_default, _cg_none, _cg_macro, _cg_function, _cg_namespace, _cg_variable, _cg_structure };
 
 struct res {
     size_t index;
@@ -35,12 +33,11 @@ struct context {
 };
 
 static void represent(size_t given, char* buffer, size_t limit, size_t* at, struct context* context) {
-    if (given < 256) { buffer[(*at)++] = given; return; }
-    else if (given - 256 >= context->name_count) return;
-    struct ent s = context->names[given - 256];
+    if (given < 256) { buffer[(*at)++] = given; return; } else given -= 256;
+    if (given >= context->name_count) return;
+    struct ent s = context->names[given];
     for (size_t i = 0; i < s.count; i++) represent(s.sig[i], buffer, limit, at, context);
-    if (!s.type) return;
-    buffer[(*at)++] = '\n';
+    if (!s.type) return; buffer[(*at)++] = '\n';
     represent(s.type, buffer, limit, at, context);
 }
 
@@ -67,9 +64,12 @@ static void parse_error(uint8_t* given, size_t given_count, uint16_t* lines, uin
 static struct res parse(uint8_t* given, size_t begin, size_t end, size_t type, size_t max_depth, struct context* C) {
     for (size_t depth = 0; depth <= max_depth; depth++) {
         for (size_t i = C->index_count; i--;) {
+            
             struct res sol = {C->indicies[i], 0, 0, 0};
             struct ent name = C->names[sol.index];
+            
             if (type && name.type != type) goto next;
+            
             for (uint8_t s = 0; s < name.count; s++) {
                 if (begin + sol.total >= end) goto next;
                 if (name.sig[s] >= 256) {
@@ -79,7 +79,7 @@ static struct res parse(uint8_t* given, size_t begin, size_t end, size_t type, s
                     sol.args = realloc(sol.args, sizeof(struct res) * (sol.count + 1));
                     sol.args[sol.count++] = arg;
                 } else if (name.sig[s] == given[begin + sol.total]) sol.total++; else goto next;
-            }
+            }            
             return sol; next: C->best = fmax(begin + sol.total, C->best);
         }
     } return (struct res) {0};
@@ -113,21 +113,27 @@ static void debug_resolved(struct res given, size_t depth, struct context* conte
 
 int main(int argc, char** argv) {
     if (argc == 1) exit(!!printf("n: \x1B[091merror:\x1B[0m no input files\n"));
+    
     for (int i = 1; i < argc; i++) {
+        
         if (argv[i][0] == '-') exit(!puts("n3zqx2l version 0.0.1\nn3zqx2l [-] [files]"));
+        
         struct context C = {
             0, 1, 0, 0,
             calloc(1, sizeof(size_t)), calloc(0, sizeof(size_t)),
             calloc(0, sizeof(struct ent)), calloc(1, sizeof(struct ent)),
         };
+        
         printf("parsing...\n");
         debug_context(&C);
+        
         FILE* file = fopen(argv[i], "r");
         if (!file) {
             fprintf(stderr, "n: %s: ", argv[i]);
             perror("error");
             continue;
         }
+        
         fseek(file, 0, SEEK_END);
         size_t L = ftell(file), count = 0;
         uint8_t *text = malloc(L), *tokens = malloc(L);
@@ -135,18 +141,35 @@ int main(int argc, char** argv) {
         fseek(file, 0, SEEK_SET);
         fread(text, 1, L, file);
         fclose(file);
+        
         uint16_t l = 1, c = 1;
         for (size_t i = 0; i < L; i++) {
             if (text[i] > 32) { lines[count] = l; columns[count] = c; tokens[count++] = text[i]; }
             if (text[i] == 10) { l++; c = 1; } else c++;
-        } struct res e = parse(tokens, 0, count, _U, 5, &C);
-        if (!e.index) parse_error(tokens, count, lines, columns, _U, &C, argv[i], text, L);
+        }
+        
+        struct res e = parse(tokens, 0, count, 256, 5, &C);
+        if (!e.index) parse_error(tokens, count, lines, columns, 256, &C, argv[i], text, L);
         debug_resolved(e, 0, &C);
         debug_context(&C);
         free(tokens);
         free(text);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -203,71 +226,6 @@ int main(int argc, char** argv) {
 //        C.names[_j0] = (struct ent) {{'0', 0}, _i, 1, 0, 0};
 //        C.names[_j1] = (struct ent) {{'0', 0}, _i, 1, 0, 0};
 //        C.names[_j] = (struct ent) {{/*'j',*/ 256+_j0, 256+_j1}, _i, 2, 0, 65536};
-         
-
-//static void duplicate_context(struct context* d, struct context* s) {
-////    d->at = s->at;
-//    d->name_count = s->name_count;
-//    d->names = realloc(d->names, sizeof(struct frame) * s->name_count);
-//    memcpy(d->names, s->names, sizeof(struct name) * s->name_count);
-//    d->frame_count = s->frame_count;
-//    d->frames = realloc(d->frames, sizeof(struct frame) * s->frame_count);
-//    memcpy(d->frames, s->frames, sizeof(struct frame) * s->frame_count);
-//}
-
-
-
-
-
-
-//static void do_intrinsic(struct context *context, const struct expr *sol) {
-//    if (sol->index == _d) {
-//
-//        size_t f = --context->frame_count;
-//
-//        context->frames[f].owner.type = sol->args[1].index;
-//
-//        context->names
-//        = realloc(context->names,
-//                  sizeof(struct name)
-//                  * (context->name_count + 1));
-//        context->names[context->name_count++]
-//        = context->frames[f].owner;
-//
-//        size_t
-//            m = context->frames[f].owner.count &&
-//                context->frames[f].owner.sig[0] >= 256,
-//            b = context->frame_count - 1;
-//
-//        context->frames[b].indicies
-//            [context->frames[b].count++]
-//            = context->name_count - 1;
-//
-//    } else if (sol->index == _a || sol->index == _p) {
-//
-//        struct name* owner =
-//        &context->frames[context->frame_count - 1].owner;
-//            owner->sig[owner->count++]
-//                = sol->index == _a
-//                    ? sol->args[0].value
-//                    : 256 + context->name_count - 1;
-//    }
-//}
-
-
-
-
-//                if (sol.index == _d) C->frame_count--;
-
-//                do_intrinsic(C, &sol);
-
-
-//                if (sol.index == _d) C->frames = realloc(C->frames, sizeof(struct frame) * ++C->frame_count);
-
-
-
-//struct frame {
-//    size_t indicies[256];   // i think we should make this:   size_t* indicies;
-//    size_t count;
-//    struct name owner;
-//};
+        
+//enum { _U = 256, _i, _n, _s, _c, _a0, _a, _p0, _p, _d0, _d1, _d, _k0, _k1, _k, _j0, _j1, _j, _intrin_count };
+//enum { _cg_default, _cg_none, _cg_macro, _cg_function, _cg_namespace, _cg_variable, _cg_structure };
