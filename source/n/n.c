a554dc34cee9e003a1f6a4fe4e41df94971d8779
dwrrehman <dwrrehman@gmail.com>
2020-05-19 21:20:10 -0700

    remove the need for a parse error function. its just a one liner now, since we arent printing the expected type now. and rightfully so.

--- source/n/n.c
+++ source/n/n.c
@@ -52,22 +52,6 @@ static inline void represent
     represent(s.type, buffer, limit, at, context);
 }
 
-static inline void parse_error
-(uint8_t* given, size_t given_count,
- uint16_t* loc, size_t type, struct context* C,
- const char* filename) {
-    if (!given_count) {
-        fprintf(stderr,"n3zqx2l: %s: error: empty file\n\n", filename);
-        return;
-    }
-    else if (C->best == given_count) C->best--;
-    char type_string[4096] = {0};
-    size_t index = 0;
-    represent(type, type_string, sizeof type_string, &index, C);
-    fprintf(stderr,"n3zqx2l: %s:%u:%u: error: %s: unresolved %c\n\n",
-           filename, loc[2*C->best], loc[2*C->best+1], type_string, given[C->best]);
-}
-
 static inline struct resolved resolve (uint8_t* given, size_t begin, size_t end, size_t type, size_t max_depth, struct context* C) {
     return (struct resolved) {0};
 }
@@ -106,24 +90,26 @@ void destroy(struct resolved r) {
 
 int main(int argc, const char** argv) {
     for (int a = 1; a < argc; a++) {
-        extern int errno;
+        
         struct stat st;
         uint8_t *text, *tokens;
         int f = open(argv[a], O_RDONLY);
+        
         if (f < 0 || stat(argv[a], &st) < 0 ||
             (text = mmap(0, st.st_size, 1, 1, f, 0))
             == MAP_FAILED) {
-            fprintf(stderr, "n3zqx2l: %s: error: %s\n",
-                    argv[a], strerror(errno)); continue;
+            fprintf(stderr, "n3zqx2l: %s: ", argv[a]);
+            perror("error"); continue;
         } else close(f);
         
-        size_t count = 0; tokens = malloc(st.st_size);
+        tokens = malloc(st.st_size);
         uint16_t* loc = malloc(4 * st.st_size), l = 1, c = 1;
+        size_t count = 0;
         
         for (size_t i = 0; i < (size_t) st.st_size; i++) {
             if (text[i] > 32) {
-                loc[2*count] = l;
-                loc[2*count+1] = c;
+                loc[2 * count] = l;
+                loc[2 * count + 1] = c;
                 tokens[count++] = text[i];
             }
             if (text[i] == 10) {
@@ -132,9 +118,15 @@ int main(int argc, const char** argv) {
             } else c++;
         }
         
-        struct context context = {0};
+        struct context context = {0};        
         struct resolved result = resolve(tokens, 0, count, 256, 5, &context);
-        if (!result.index) parse_error(tokens, count, loc, 256, &context, argv[a]);
+        
+        if (!result.index && count) {
+            if (context.best == count) context.best--;
+            fprintf(stderr,"n3zqx2l: %s:%u:%u: error: unresolved %c\n\n",
+                    argv[a], loc[2 * context.best],
+                    loc[2 * context.best + 1], tokens[context.best]);
+        }
         
         debug_resolved(result, 0, &context);
         debug_context(&context);
