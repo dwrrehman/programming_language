31ac2127d346d2b081541c7c060e13027b556323
dwrrehman <dwrrehman@gmail.com>
2021-01-28 19:21:24 -0800

    start the process of coming up with the backtracking universal call syntax algorithm. i think its very promising and correct, and simple.

--- n.c
+++ n.c
@@ -4,96 +4,37 @@
 #include <string.h>
 #include <unistd.h>
 
+int main() {
 
-int main() {s
-	const char* initial = "bob\ncat\ndaniel\n", * input = "daniel";
-	
-	int
-		length = (int) strlen(input),
+	const char* A = "daniel";
+	const char* B = "\nbob\ncat\ndaniel\n";
+	const int Al = strlen(A), Bl = strlen(B);
 
-		program_limit = 4096, 
-		context_limit = 4096, 
-		argument_limit = 4096, 
-		stack_limit = 4096,
+	int a = 0, b = 0, c = 0, d = 0, e = 0;
+	int C[4096] = {0};
 
-		context_count = (int) strlen(initial),
-		program_count = 0, 
-		arg = 0, 
-		top = 0, 
-		begin = 0, 
-		count = 0, 
-		index = 0, 
-		element = 0;
-	
-	int* program = malloc(program_limit * sizeof(int));
-	int* arguments = malloc(argument_limit * sizeof(int));
-	int* stack = malloc(stack_limit * sizeof(int));
-	char* context = malloc(context_limit);
+	while (a < Al and A[a] < 33) a++;
 
-	strcpy(context, initial);
+	C[c + 0] = a;
+	C[c + 1] = b;
 
-	printf("input : <<<%s>>>\n", input);
-	printf("context : <<<%s>>>\n", context);
-
-	while (begin < length and input[begin] < 33) begin++;
-
-	stack[top] = 0;
-	stack[top + 3] = begin;
-try:
-	if (index == context_count) {
-		if (not top) goto error;
-		top -= 8; 
-		goto try;
+_0:
+	if (b == Bl) {
+		if (not c) return 1;
+		c -= 8; 
+		goto _0;
 	}
-	count = 0;
-	begin = stack[top + 3];
-parent:
-	element = context[index++];
-	if (element == ' ') {
-		top += 8;
-		stack[top + 1] = index;
-		stack[top + 3] = begin;
-		stack[top + 5] = count;
-		stack[top + 6] = arg;
-		arg += count;
-		goto try;
-	}
+	do b++; while (b < Bl and B[b] != '\n');
+	a = C[c];
+_1:
+	e = B[b++];
+	if (e == 32) { goto _0; }
+	if (a >= Al or e != A[a]) goto _0;
+	do a++; while (a < Al and A[a] < 33);
+	if (B[b] != '\n') goto _1;
+	if (c) { goto _1; }
+	if (a != Al) goto _0;
 
-	if (begin >= length or element != input[begin]) goto try;
-
-	do begin++; while (begin < length and input[begin] < 33);
-
-	if (context[index] != '\n') goto parent;
-
-	program[program_count] = index;
-	program[program_count + 1] = count;
-	memcpy(program + program_count + 2, arguments + arg, sizeof(int) * (size_t) count);
-
-	if (top) {
-		int save = count;
-		index = stack[top + 1];
-		count = stack[top + 5];
-		arg = stack[top + 6];
-		arguments[arg + count] = program_count;
-		count++;
-		top -= 8;
-		program_count += 2 + save;
-		goto parent;
-	}
-
-	if (begin != length) goto try;
-
-	printf("\n\tcompile successful.\n\n");
-
-	goto final;
-error:
-	printf("\n\tcompile failure.\n\n");
-final:
-	for (int i = 0; i < program_count + count + 10; i++)
-		printf("%d ", program[i]);
-	free(context);
-	free(program);
-	free(arguments);
-	free(stack);
+	puts("compile successful.");
 }
 
