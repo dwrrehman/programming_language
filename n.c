ab5bdb40f96f2cf4f17edcc159dacf75129b7fa4
dwrrehman <dwrrehman@gmail.com>
2021-01-29 18:34:21 -0800

    make the compiler use only if and goto, and realized that the fundemental data structures are just the stack (output) input, and context. super simple.

--- n.c
+++ n.c
@@ -2,39 +2,42 @@
 #include <iso646.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
 int main() {
 
-	const char* A = "daniel";
-	const char* B = "\nbob\ncat\ndaniel\n";
-	const int Al = strlen(A), Bl = strlen(B);
+	const char* input = "daniel";
+	const char* context = "\nbob\ncat\ndaniel\n";
+	const int length = (int) strlen(input), count = (int) strlen(context);
 
-	int a = 0, b = 0, c = 0, d = 0, e = 0;
-	int C[4096] = {0};
+	int a = 0, b = 0, c = 0, d = 0;
+	int stack[4096] = {0};
 
-	while (a < Al and A[a] < 33) a++;
+	while (a < length and input[a] < 33) a++;
 
-	C[c + 0] = a;
-	C[c + 1] = b;
+	stack[c + 0] = a; // ranges in input length
+	stack[c + 1] = b; // ranges in context length
+	stack[c + 2] = c; // ranges in MAX context length   // is this really necccessary?...
+	stack[c + 3] = d; // ranges in stack
 
-_0:
-	if (b == Bl) {
+_0:	if (b == count) {
 		if (not c) return 1;
-		c -= 8; 
+		c -= 2; 
 		goto _0;
 	}
-	do b++; while (b < Bl and B[b] != '\n');
-	a = C[c];
-_1:
-	e = B[b++];
-	if (e == 32) { goto _0; }
-	if (a >= Al or e != A[a]) goto _0;
-	do a++; while (a < Al and A[a] < 33);
-	if (B[b] != '\n') goto _1;
-	if (c) { goto _1; }
-	if (a != Al) goto _0;
+_1:	b++; 
+	if (b < count and context[b] != 10) goto _1;
+	a = stack[c];
+_2:	if (context[b] == 32) {
+		goto _0;
+	}
+	if (a >= length) goto _0;
+	if (context[b] != input[a]) goto _0;
+_3:	a++;
+	if (a < length and input[a] < 33) goto _3;
+	b++;
+	if (context[b] != 10) goto _2;
+	if (c) { goto _2; }
+	if (a != length) goto _0;
 
 	puts("compile successful.");
 }
-
