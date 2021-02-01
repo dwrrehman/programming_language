938d0a85a9f2eb3a9f10c915ddb16b4e4521b4e6
dwrrehman <dwrrehman@gmail.com>
2021-02-01 08:53:02 -0800

    rework the btucsr alg a bit, and try to get rid of parent tenatively.

--- n.c
+++ n.c
@@ -5,77 +5,111 @@
 
 int main() {
 
-	const char* input = "daniel";
-	const char* context = "\nbob\ncat\ndaniel\n";
-	const int length = (int) strlen(input), count = (int) strlen(context);
+	const char* A = "daniel";
+	const char* B = "\nbob\ncat \ndaniel\n";
+	const int al = (int) strlen(in), bl = (int) strlen(env);
 
-	int a = 0, b = 0, c = 0, d = 0;
-	int stack[4096] = {0};
+	int a = 0, b = 0, c = 0, d = 4096;
+	int C[4096] = {0};
 
-	while (a < length and input[a] < 33) a++;
+	while (a < al and A[a] < 33) a++;
+	C[c] = a;
 
-	stack[c + 0] = a; // ranges in input length
-	stack[c + 1] = b; // ranges in context length
-	stack[c + 2] = c; // ranges in MAX context length   // is this really necccessary?...
-	stack[c + 3] = d; // ranges in stack
-
-_0:	if (b == count) {
-		if (not c) return 1;
+_0:	if (b == bl) {
+		if (!c) goto error;
 		c -= 2; 
+		b = C[c + 1];
 		goto _0;
 	}
-_1:	b++; 
-	if (b < count and context[b] != 10) goto _1;
-	a = stack[c];
-_2:	if (context[b] == 32) {
+	while (b < bl and B[b] != 10) b++; b++; 
+	a = C[c];
+_2:	if (B[b] == 32) {
+		C[c + 1] = b;
+		C[c + 2] = a;
+		c += 2;
+		b = 0;
 		goto _0;
 	}
-	if (a >= length) goto _0;
-	if (context[b] != input[a]) goto _0;
-_3:	a++;
-	if (a < length and input[a] < 33) goto _3;
-	b++;
-	if (context[b] != 10) goto _2;
-	if (c) { goto _2; }
-	if (a != length) goto _0;
+	if (a >= al) goto _0;
+	if (A[a] != B[b]) goto _0;
+	do a++; while (a < al and A[a] < 33);
+	b++; 
+	if (B[b] != 10) goto _2;
+
+	if (d != -1) { 
+		C[c + 1] = b;
+		C[c + 2] = a;
+		c += 2;
+		b = C[d + 1] + 1;
+		goto _2; 
+	}
+	if (a != al) goto _0;
 
 	puts("compile successful.");
+	return 0;
+error:
+	puts("compile error.");
+	return 1;
 }
 
-void btucsr() {
-try:
-	if (b == C_length) {
-		if (c == 0) return error;
-		c -= 3;
-		b = C[c + 1];
-		goto try;
-	}
-	while (B[b] != 10) b++; b++;
-	a = C[c];
-parent:
-	if (B[b] == 32) {
-		C[c + 1] = b;
-		C[c + 3] = a;
-		C[c + 5] = c;
-		b = 0;
-		c += 3;
-		goto try;
-	}
-	if (B[b] != A[a]) goto try;
-	do a++; while (A[a] < 33);
-	b++;
-	if (B[b] != 10) goto parent;
 
-	int d = C[c + 2];
-	if (d != -1) {
-		C[c + 1] = b;
-		C[c + 3] = a;
-		C[c + 5] = C[d + 2];
-		c += 3;
-		b = C[d + 1] + 1;
-		goto parent;
-	}
-	if (a != A_length) goto try;
-	return parse_success, {C, c};
-}
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ---------------- garbage ----------------------------
+
+
+ 	// ranges in input length
+	//stack[c + 1] = b; // ranges in context length
+	// stack[c + 2] = c; // ranges in MAX context length   // is this really necccessary?...
+	// stack[c + 3] = d; // ranges in stack
+
+
+// void btucsr() {
+// try:
+// 	if (b == C_length) {
+// 		if (c == 0) return error;
+// 		c -= 3;
+// 		b = C[c + 1];
+// 		goto try;
+// 	}
+// 	while (B[b] != 10) b++; b++;
+// 	a = C[c];
+// parent:
+// 	if (B[b] == 32) {
+// 		C[c + 1] = b;
+// 		C[c + 3] = a;
+// 		C[c + 5] = c;
+// 		b = 0;
+// 		c += 3;
+// 		goto try;
+// 	}
+// 	if (B[b] != A[a]) goto try;
+// 	do a++; while (A[a] < 33);
+// 	b++;
+// 	if (B[b] != 10) goto parent;
+
+// 	int d = C[c + 2];
+// 	if (d != -1) {
+// 		C[c + 1] = b;
+// 		C[c + 3] = a;
+// 		C[c + 5] = C[d + 2];
+// 		c += 3;
+// 		b = C[d + 1] + 1;
+// 		goto parent;
+// 	}
+// 	if (a != A_length) goto try;
+// 	return parse_success, {C, c};
+// }
