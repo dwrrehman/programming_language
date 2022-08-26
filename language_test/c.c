48fa2fffad00868a1605f71b40b4d84b8fd09064
Daniel Rehman <dwrrehman@gmail.com>
2022-08-26 13:44:15 -0700

    clean up the code a ton, and prepare to add branches and labels soon.

--- language_test/c.c
+++ language_test/c.c
@@ -3,6 +3,7 @@
 
 /*
 
+
 ------------------------ domain --------------------------
 
 
@@ -16,12 +17,14 @@
 
 ---------------------------- priorities of the language ------------------------------
 
+		ranked in order of importance!
+
 
 	1. performance: maximal runtime performance
 
 	2. simplicity: extreme minimalism and simplicity
 
-	3. flexibility: good language/compiletime flexibility 
+	3. flexibility: great language flexibility, very general compiletime 
 
 	4. readability: decent programmer abstractions for readability
 
@@ -96,7 +99,41 @@
 
 
 
+
+
+
+
+
+
+
+		// we are in the middle of implementing labels and gotos and branches. 
+
+
+
+
+
+			// note: we totally dont even need the bool in_use array,because we can just use the name being a null pointer or not. and when we are done with a register, just set its name to be null. thats it. thats much better. 
+
+			//   and then the general algorithm for the     defining new register section 
+
+			// would be to just find an open null pointer in the array of names 
+				// if you went through the entire array of names, and didnt find anything, then you know you ran out of registers, i think.
+
+			// also when going through the array, you want to make sure the name  you are checking against, in the table is not a null pointer, of course. yeah this is good. 
+
+
+
 */
+
+
+
+
+
+
+
+
+
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <iso646.h>
@@ -133,8 +170,6 @@ enum op_code {
 };
 
 
-
-
 static const char* op_code_spelling[] = {
 	"op_nop",
 	"op_ct_xor",
@@ -156,14 +191,12 @@ static const char* op_code_spelling[] = {
 };
 
 
-
 struct instruction {
 	nat op;
 	nat _0;
 	nat _1;
-	nat _2; // we should make this an array,  and make it 32 long. ish. or more.
+	nat _2; 
 };
-
 
 static char* read_file(const char* filename, size_t* out_length) {
 	const int file = open(filename, O_RDONLY);
@@ -188,265 +221,212 @@ static char* read_file(const char* filename, size_t* out_length) {
 }
 
 
-
-
-static bool alive[128] = {0};
-static char* names[128] = {0};
-static nat registers[128] = {0};
-static nat ct_registers[128] = {0};
-static nat count = 0;
-static struct instruction I = {0};
-static nat base = 0;
-
-static nat pc = 0; // program counter 
-
-static char* label_names[2048] = {0};
-static nat labels[2048] = {0};
-
-static uint64_t* memory = NULL;
-
 static bool equal(const char* s1, const char* s2) {
 	if (strlen(s1) != strlen(s2)) return false;
 	else return not strcmp(s1, s2);
 }
 
 
-static void compile_in(const char* string, nat this_location) {
+
+
+static struct instruction instructions[4096] = {0};
+static nat instruction_count = 0,
+
+static nat _[4] = {0};
+static nat base = 0;
+
+
+static char* names[128] = {0};
+static nat name_count = 0;
+
+static char* label_names[2048] = {0};
+static nat labels[2048] = {0};
+
+
+
+
+
+static void ins(nat op) {
+	instructions[instruction_count++] = (struct instruction) {
+		.op = op, ._0 = _[0], ._1 = _[1], ._2 = _[2]
+	};
+}
+
+static void print_ins(struct instruction O, nat pc) {
+	printf("---> [%llu] instruction: { operation=%s, dest=%llu, first=%llu, second=%llu }\n", 
+		pc, op_code_spelling[O.op], O._0, O._1, O._2);
+}
+
+
+static void compile_in(const char* string) {
 	
 	if (base) {
-		ct_registers[I._0] = (nat) strtoll(string, NULL, (int) base);
+		ct_registers[*_] = (nat) strtoll(string, NULL, (int) base);
 		base = 0;
 		return;
 	}
 
 	if (equal(string, "pass")) {}
 
-	else if (equal(string, "all")) { 		// 000
-		nat t0 = I._0; 
-		I._0 = t0; I._1 = t0; I._2 = t0;
-	}
-	else if (equal(string, "dup")) { 		// 001
-		nat t0 = I._0, t1 = I._1;
-		I._0 = t0; I._1 = t0; I._2 = t1;
-	}
-	else if (equal(string, "dup0")) { 		// 002
-		nat t0 = I._0, t2 = I._2;
-		I._0 = t0; I._1 = t0; I._2 = t2;
-	}
-	else if (equal(string, "cycle+")) { 		// 201
-		nat t0 = I._0, t1 = I._1, t2 = I._2; 
-		I._0 = t2; I._1 = t0; I._2 = t1;
-	}
-	else if (equal(string, "cycle-")) { 		// 120
-		nat t0 = I._0, t1 = I._1, t2 = I._2; 
-		I._0 = t1; I._1 = t2; I._2 = t0;
-	}
-	else if (equal(string, "switch")) { 		// 021
-		nat t0 = I._0, t1 = I._1, t2 = I._2; 
-		I._0 = t0; I._1 = t2; I._2 = t1;
-	}
-	else if (equal(string, "dup1")) { 		// 112
-		nat t1 = I._1, t2 = I._2; 
-		I._0 = t1; I._1 = t1; I._2 = t2;
-	}
-	else if (equal(string, "exchange")) {  		// 102
-		nat t0 = I._0, t1 = I._1, t2 = I._2; 
-		I._0 = t1; I._1 = t0; I._2 = t2;
-	}
-	else if (equal(string, "back")) {  		// 210
-		nat t0 = I._0, t1 = I._1, t2 = I._2; 
-		I._0 = t2; I._1 = t1; I._2 = t0;
-	}
-	else if (equal(string, "copy")) {		// 022
-		nat t0 = I._0, t2 = I._2; 
-		I._0 = t0; I._1 = t2; I._2 = t2;
-	}
+	else if (equal(string, "000")) { _[1] = _[0]; _[2] = _[0]; }
+	else if (equal(string, "001")) {nat t1 = _[1]; _[1] = _[0]; I._2 = t1;}
 
+	else if (equal(string, "021")) { nat t1 = _[1], t2 = _[2]; _[1] = t2; _[2] = t1; }
+	else if (equal(string, "10")) { nat t0 = _[0], t1 = _[1]; _[0] = t1; _[1] = t0; }
+	else if (equal(string, "210")) { nat t0 = _[0], t2 = _[2]; _[0] = t2; _[2] = t0; }
 	
+	else if (equal(string, "201")) {
+		nat t0 = _[0], t1 = _[1], t2 = _[2]; _[0] = t2; _[1] = t0; _[2] = t1;
+	}
+	else if (equal(string, "120")) {
+		nat t0 = _[0], t1 = _[1], t2 = _[2]; _[0] = t1; _[1] = t2; _[2] = t0;
+	}
 
-	else if (equal(string, "slt")) I.op = op_slt;
-	else if (equal(string, "slti")) I.op = op_slti;
+	else if (equal(string, "11")) { _[0] = _[1]; }
+	else if (equal(string, "21")) { _[0] = _[2]; }
+	else if (equal(string, "00")) { _[1] = _[0]; }
+	else if (equal(string, "02")) { _[1] = _[2]; }
 
-	else if (equal(string, "xor")) I.op = op_xor;
-	else if (equal(string, "ct_xor")) I.op = op_ct_xor;	
-
-	else if (equal(string, "add")) I.op = op_add;
-	else if (equal(string, "addi")) I.op = op_addi;
-	else if (equal(string, "ct_add")) I.op = op_ct_add;
-	
-	else if (equal(string, "sub")) I.op = op_sub;
-	else if (equal(string, "ct_sub")) I.op = op_ct_sub;
-
-	else if (equal(string, "sll")) I.op = op_sll;
-	else if (equal(string, "slli")) I.op = op_slli;
-
-	else if (equal(string, "load64")) I.op = op_load64;
-
-	else if (equal(string, "print")) I.op = op_debug;
-	else if (equal(string, "ctprint")) I.op = op_ct_debug;
-
+	else if (equal(string, "slt")) ins(op_slt);
+	else if (equal(string, "slti")) ins(op_slti);
+	else if (equal(string, "xor")) ins(op_xor);
+	else if (equal(string, "ct_xor")) ins(op_ct_xor);
+	else if (equal(string, "add")) ins(op_add);
+	else if (equal(string, "addi")) ins(op_addi);
+	else if (equal(string, "ct_add")) ins(op_ct_add);
+	else if (equal(string, "sub")) ins(op_sub);
+	else if (equal(string, "ct_sub")) ins(op_ct_sub);
+	else if (equal(string, "sll")) ins(op_sll);
+	else if (equal(string, "slli")) ins(op_slli);
+	else if (equal(string, "load64")) ins(op_load64);
+	else if (equal(string, "blt")) ins(op_blt);
+	else if (equal(string, "print")) ins(op_debug);
+	else if (equal(string, "ctprint")) ins(op_ct_debug);
 	else if (equal(string, "hliteral")) { base = 16; }
 	else if (equal(string, "dliteral")) { base = 10; }
 	else if (equal(string, "bliteral")) { base = 2; }
-
-	else if (equal(string, "delete")) {
-		if (alive[I._0]) {
-			free(names[I._0]);
-			alive[I._0] = false;
-		} else {
-			printf("error: register is not allocated.\n");
-		}
-	}
+	else if (equal(string, "delete")) { free(names[*_]); names[*_] = NULL; }
 
 	else if (equal(string, "here")) {
-		printf("assigning here location: %llu\n", this_location);
-		ct_registers[I._0] = this_location;
 
-
-		// we are in the middle of implementing labels and gotos and branches. 
-
-
-	} else if (equal(string, "blt")) {
-		I.op = op_blt;
-	}
-
-	else {
-		nat i = 0;
-		while(i < count) {
-			if (alive[i] and equal(string, names[i])) break;
-			i++;
-		}
-
-		if (i == count) {
-			printf("defining new _0 register: %s\n", string);
-			
-			nat j = 0;
-			while (j < count) {
-				if (not alive[j]) break;
-				j++;
+		printf("assigning here location: %llu\n", instruction_count);
+		ct_registers[*_] = instruction_count;
+	
+	} else {
+		nat name = 0;
+		nat open = name_count;
+		while (name < name_count) {
+			if (names[name]) {
+				if (equal(string, names[name])) break;
+			} else if (open == name_count) {
+				open = name;
 			}
-
-			if (j == count) count++;
-			names[j] = strdup(string);
-
-			I._2 = I._1;
-			I._1 = I._0;
-			I._0 = j;
-			alive[j] = true;
-		} else {
-			printf("found existing _0 register: %s [%llu]\n", string, i);
-			I._2 = I._1;
-			I._1 = I._0;
-			I._0 = i;
+			name++;
 		}
+
+		if (name == name_count) {
+			printf("new: %s\n", string);
+			if (open == name_count) name_count++;
+			names[open] = strdup(string);
+			name = open;
+		} 
+		_[2] = _[1]; _[1] = _[0]; _[0] = name;
 	}
 }
+
+
+static uint64_t* memory = NULL;
+static nat registers[128] = {0};
+static nat ct_registers[128] = {0};
+static nat pc = 0;
+
 
 static void execute_ct_instruction(struct instruction I) {
+	if (I.op == op_nop) {}
+	else if (I.op == op_ct_xor) ct_registers[I._0] = ct_registers[I._1] ^ ct_registers[I._2];
+	else if (I.op == op_ct_add) ct_registers[I._0] = ct_registers[I._1] + ct_registers[I._2];
+	else if (I.op == op_ct_sub) ct_registers[I._0] = ct_registers[I._1] - ct_registers[I._2];
+	else if (I.op == op_ct_debug) printf("CT#%llu=%llu\n", I._0, ct_registers[I._0]);
 
-	if (I.op == op_nop) {
-		printf("executing: ct_NOP[r%llu,r%llu,r%llu]\n", I._0,I._1,I._2);
-		pc++;
+	else { 
+		// then it must be a rt instruction. generate it!
 	}
+	pc++;
+}
 
-	else if (I.op == op_ct_xor) {
-		printf("executing: ct_xor[c%llu,c%llu,c%llu]\n", I._0,I._1,I._2);
-		ct_registers[I._0] = ct_registers[I._1] ^ ct_registers[I._2];
-		pc++;
-	}
-
-	else if (I.op == op_ct_add) {
-		printf("executing: ct_add[c%llu,c%llu,c%llu]\n", I._0,I._1,I._2);
-		ct_registers[I._0] = ct_registers[I._1] + ct_registers[I._2];
-		pc++;
-	}
-
-	else if (I.op == op_ct_sub) {
-		printf("executing: ct_sub[c%llu,c%llu,c%llu]\n", I._0,I._1,I._2);
-		ct_registers[I._0] = ct_registers[I._1] - ct_registers[I._2];
-		pc++;
-	}
-
-	else if (I.op == op_ct_debug) {
-		printf("CT#%llu=%llu\n", I._0, ct_registers[I._0]);
-		pc++;
-	}
+static void execute_instruction(struct instruction I) {
+	if (I.op == op_nop) {}
+	else if (I.op == op_xor) registers[I._0] = registers[I._1] ^ registers[I._2];
+	else if (I.op == op_add) registers[I._0] = registers[I._1] + registers[I._2];
+	else if (I.op == op_sub) registers[I._0] = registers[I._1] - registers[I._2];
+	else if (I.op == op_addi) registers[I._0] = registers[I._1] + ct_registers[I._2];
+	else if (I.op == op_slt) registers[I._0] = registers[I._1] < registers[I._2];
+	else if (I.op == op_slti) registers[I._0] = registers[I._1] < ct_registers[I._2];
+	else if (I.op == op_sll) registers[I._0] = registers[I._1] << registers[I._2];
+	else if (I.op == op_slli) registers[I._0] = registers[I._1] << ct_registers[I._2];
+	else if (I.op == op_load64) registers[I._0] = *(((uint64_t*)memory) + (registers[I._1] + ct_registers[I._2])); 
+	else if (I.op == op_blt) { if (registers[I._0] < registers[I._1]) pc += ct_registers[I._2]; }
+	else if (I.op == op_debug) printf("R#%llu=%llu\n", I._0, registers[I._0]);
+	pc++;
 }
 
 
 
+static void interpret_file() {
 
-static void execute_instruction(struct instruction I) {
+	printf("file: ");
+	fgets(buffer, sizeof buffer, stdin);
+	buffer[strlen(buffer) - 1] = 0;
 
-	if (I.op == op_nop) {
-		printf("executing: NOP[r%llu,r%llu,r%llu]\n", I._0,I._1,I._2);
-		pc++;
+	size_t text_length = 0;
+	char* text = read_file(buffer, &text_length);
+
+
+	const char delimiter = '.';    
+
+
+	char* code[1024] = {0};
+	char b[4096] = {0};
+	nat len = 0;
+
+	nat count = 0;
+	for (nat i = 0; i < text_length; i++) {
+		if (text[i] == delimiter) {
+			b[len] = 0; len = 0;
+			code[count++] = strdup(b);
+		} else if (text[i] >= 33) b[len++] = text[i];
 	}
 
-	else if (I.op == op_xor) {
-		printf("executing: xor[r%llu,r%llu,r%llu]\n", I._0,I._1,I._2);
-		registers[I._0] = registers[I._1] ^ registers[I._2];
-		pc++;
+	printf("file statements: ");
+	for (nat i = 0; i < count; i++) {
+		printf("\t%s\n", code[i]);
 	}
 
-	else if (I.op == op_add) {
-		printf("executing: add[r%llu,r%llu,r%llu]\n", I._0,I._1,I._2);
-		registers[I._0] = registers[I._1] + registers[I._2];
-		pc++;
+	nat ins_count = 0;
+	struct instruction ct_instructions[1024] = {0};
+
+	nat ins_count = 0;
+	struct instruction rt_instructions[1024] = {0};
+
+	printf("----------------------- compiling... -------------------\n");
+	for (nat i = 0; i < count; i++) {
+		printf(": parsing: %s\n", code[i]);
+		compile_in(code[i]);
 	}
 
-	else if (I.op == op_sub) {
-		printf("executing: sub[r%llu,r%llu,r%llu]\n", I._0,I._1,I._2);
-		registers[I._0] = registers[I._1] - registers[I._2];
-		pc++;
+	nat pc = 0; 
+
+	printf("------------------- CT interpreting... -------------------\n");
+	while (pc < ins_count) {
+		print_ins(instructions[i], pc);
+		execute_ct_instruction(instructions[i]);
 	}
 
-	else if (I.op == op_addi) {
-		printf("executing: addi[r%llu,r%llu,c%llu]\n", I._0,I._1,I._2);
-		registers[I._0] = registers[I._1] + ct_registers[I._2];
-		pc++;
-	}
-
-	else if (I.op == op_slt) {
-		printf("executing: slt[r%llu,r%llu,r%llu]\n", I._0,I._1,I._2);
-		registers[I._0] = registers[I._1] < registers[I._2];
-		pc++;
-	}
-
-	else if (I.op == op_slti) {
-		printf("executing: slt[r%llu,r%llu,r%llu]\n", I._0,I._1,I._2);
-		registers[I._0] = registers[I._1] < ct_registers[I._2];
-		pc++;
-	}
-
-	else if (I.op == op_sll) {
-		printf("executing: sll[r%llu,r%llu,r%llu]\n", I._0,I._1,I._2);
-		registers[I._0] = registers[I._1] << registers[I._2];
-		pc++;
-	}
-
-	else if (I.op == op_slli) {
-		printf("executing: slli[r%llu,r%llu,c%llu]\n", I._0,I._1,I._2);
-		registers[I._0] = registers[I._1] << ct_registers[I._2];
-		pc++;
-	}
-
-	else if (I.op == op_load64) {
-		printf("executing: load64[r%llu,r%llu,c%llu]\n", I._0,I._1,I._2);
-		registers[I._0] = *(((uint64_t*)memory) + (registers[I._1] + ct_registers[I._2])); 
-		pc++;
-	}
-
-
-	else if (I.op == op_blt) {
-		printf("executing: blt[r%llu,r%llu,label=%llu]\n", I._0,I._1,I._2);
-		if (registers[I._0] < registers[I._1]) pc += ct_registers[I._2];
-		else pc++;
-	}
-	
-	else if (I.op == op_debug) {
-		printf("R#%llu=%llu\n", I._0, registers[I._0]);
-		pc++;
+	pc = 0;
+	printf("---------------- interpreting... -----------------------\n");
+	while (pc < rt_ins_count) {
+		print_ins(rt_instructions[i], pc);
+		execute_instruction(rt_instructions[i]);
 	}
 }
 
@@ -454,6 +434,7 @@ static void execute_instruction(struct instruction I) {
 int main() { 
 
 	printf("a repl/interpreter for my language. \ndwrr started on 2208232.211844 \n");
+
 
 	memory = aligned_alloc(8, 1 << 16);
 
@@ -472,80 +453,7 @@ _: 	printf(" • ");
 	if (equal(string, "")) {}
 	else if (equal(string, "o")) printf("\033[H\033[J");
 	else if (equal(string, "quit")) goto done;
-	
-	else if (equal(string, "file")) {
-		printf("file: ");
-
-		fgets(buffer, sizeof buffer, stdin);
-		buffer[strlen(buffer) - 1] = 0;
-	
-		size_t text_length = 0;
-		char* text = read_file(buffer, &text_length);
-
-		char* code[1024] = {0};
-		char b[4096] = {0};
-		nat len = 0;
-
-		nat count = 0;
-		for (nat i = 0; i < text_length; i++) {
-			if (text[i] == '.') {
-				b[len] = 0;
-				len = 0;
-				code[count++] = strdup(b);
-			}
-			else if (text[i] >= 33) {
-				b[len++] = text[i];
-			}
-		}
-
-		
-		
-		printf("code: ");
-		for (nat i = 0; i < count; i++) {
-			printf("\t%s\n", code[i]);
-		}
-
-		nat ins_count = 0;
-		struct instruction instructions[1024] = {0};
-
-		printf("-----------------------compiling...-------------------\n");
-		for (nat i = 0; i < count; i++) {
-			printf(": parsing: %s\n", code[i]);
-
-			compile_in(code[i], ins_count);
-
-			if (I.op != op_nop) 
-				instructions[ins_count++] = I;
-			I.op = op_nop;
-		}
-
-
-
-
-		printf("-------------------CT interpreting...-------------------\n");
-		for (pc = 0; pc < ins_count; ) {
-			struct instruction O = instructions[i];
-			
-			printf("---> [%llu] CT exec ins: { operation=%s, dest=%llu, "
-				"first=%llu, second=%llu }\n",
-				 pc, op_code_spelling[O.op], O._0, O._1, O._2);
-		
-			execute_ct_instruction(O);
-		}
-
-
-
-		printf("----------------RT interpreting...-----------------------\n");
-		for (pc = 0; pc < ins_count;) {
-			struct instruction O = instructions[i];
-			
-			printf("---> [%llu] RT exec ins: { operation=%s, dest=%llu, "
-				"first=%llu, second=%llu }\n",
-				 pc, op_code_spelling[O.op], O._0, O._1, O._2);
-		
-			execute_instruction(O);
-		}
-	}
+	else if (equal(string, "file")) interpret_file();
 
 
 	else if (not strcmp(string, "help"))
@@ -622,6 +530,33 @@ done:
 
 	printf("quitting...\n");
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
