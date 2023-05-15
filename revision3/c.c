20517267769f68fb665febf6a7c882d73bb5a221
Daniel Rehman <dwrrehman@gmail.com>
2023-05-15 16:26:07 -0700

    push the current code for the interpreter, trying to figure out the argument passing method or revision currently. variables are memory and register capable now, not just register.

--- revision3/c.c
+++ revision3/c.c
@@ -10,39 +10,84 @@
 #include <sys/mman.h>
 typedef size_t nat;
 
-static void print_word(char* string, nat start, nat count) {
-	for (nat i = 0; i < count; i++) putchar(string[i + start]);
+enum word_type {
+	null_word,
+	label_definition,
+	variable_definition,
+};
+
+enum instruction_type {
+	nop, 
+	add,
+	bne,
+};
+
+
+
+
+
+struct word {
+	char* name;
+	nat length;
+	nat type;
+};
+
+struct instruction {
+	nat operation;
+	nat arg0;
+	nat arg1;
+	nat arg2;
+};
+
+static nat arguments[16] = {0};
+static nat dictionary_count = 0;
+static struct word* dictionary = NULL;
+static nat instruction_count = 0;
+static struct instruction* instructions = NULL;
+
+static void ins(nat op, nat arg0, nat arg1, nat arg2) {
+	instructions = realloc(instructions, sizeof(struct instruction) * (instruction_count + 1));
+	instructions[instruction_count++] = (struct instruction){op, arg0, arg1, arg2};
 }
 
 static void interpret(char* string, nat length) {
+	
 	nat count = 0, start = 0;
 	for (nat i = 0; i < length; i++) {
 		if (not isspace(string[i])) { 
 			if (not count) start = i; 
 			count++; continue; 
 		} else if (not count) continue;
-		here:; 
+		process_word:; 
 		char* word = string + start;
 
-		if (not strncmp(word, "hello", count)) {
-			printf("executing hello command...\n");
+		     if (not strncmp(word, "nop", count)) ins(nop,0,0,0);
+		else if (not strncmp(word, "add", count)) ins(add,0,0,0);
+		else if (not strncmp(word, "bne", count)) ins(bne,0,0,0);
 
-		} else if (not strncmp(word, "there", count)) {
-			printf("executing there command...\n");
-
-		} else {
-			printf("found user-defined variable: "); 
-			print_word(string, start, count); 
+		else {
+			printf("found label: "); 
+			for (nat _ = 0; _ < count; _++) putchar(string[_ + start]);
 			puts("");
+
+			for (nat entry = 0; entry < dictionary_count; entry++) {
+				if (strncmp(dictionary[entry].name, word, count)) continue;
+				printf("found existing label!\n");
+				goto found;
+			}
+
+			printf("making a new label (Adding to dict!)....\n");
+			dictionary = realloc(dictionary, sizeof(struct word) * (dictionary_count + 1));
+			dictionary[dictionary_count++] = (struct word) {.name = strndup(word, count), .length = count, .type = label_definition};
+			found:;
 		}
 		count = 0;
 	}
-
-	if (count) goto here;
+	if (count) goto process_word;
 }
 
 int main() {
-	puts("a repl for my programminglanguage.");
+	puts("a repl for my programming language.");
 	char line[4096] = {0};
 loop: 	printf(" : ");
 	fgets(line, sizeof line, stdin);
