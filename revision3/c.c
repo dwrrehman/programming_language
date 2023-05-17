8ead2cd4f516c260d69efcf15d3fc6acfe7bf2df
Daniel Rehman <dwrrehman@gmail.com>
2023-05-16 21:47:14 -0700

    push the current code. i got label location attribution working, without even requiring that user say here at all. its works automatically. pretty cool. everythings a label until proven otherwise, basically.

--- revision3/c.c
+++ revision3/c.c
@@ -7,79 +7,204 @@
 #include <ctype.h>
 #include <fcntl.h>
 #include <sys/stat.h>
-#include <sys/mman.h>
+#include <sys/mman.h>        
+
+#define red   "\x1B[31m"
+#define green   "\x1B[32m"
+#define yellow   "\x1B[33m"
+#define blue   "\x1B[34m"
+#define magenta   "\x1B[35m"
+#define cyan   "\x1B[36m"
+#define reset "\x1B[0m"
+
+#define debug 1
+
 typedef size_t nat;
+enum word_type { nullw, generic_def, generic2_def, label_def, label2_def, forward_def, backward_def, var_def };
+enum instruction_type { nulli, nop, add, _xor, bne };
+struct word { char* name; nat length, type, value; };
 
-enum word_type {
-	null_word,
-	label_definition,
-	variable_definition,
-};
+static nat arguments[32] = {0};
 
-enum instruction_type {
-	nop, 
-	add,
-	bne,
-};
-
-
-
-
-
-struct word {
-	char* name;
-	nat length;
-	nat type;
-};
-
-struct instruction {
-	nat operation;
-	nat arg0;
-	nat arg1;
-	nat arg2;
-};
-
-static nat arguments[16] = {0};
 static nat dictionary_count = 0;
 static struct word* dictionary = NULL;
-static nat instruction_count = 0;
-static struct instruction* instructions = NULL;
 
-static void ins(nat op, nat arg0, nat arg1, nat arg2) {
-	instructions = realloc(instructions, sizeof(struct instruction) * (instruction_count + 1));
-	instructions[instruction_count++] = (struct instruction){op, arg0, arg1, arg2};
+static nat ins_count = 0;
+static nat* instructions = NULL;
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+static const char* spell_type(nat t) { 
+	if (t == nullw) return "{null}";
+
+	if (t == generic_def) return yellow "generic_def" reset;
+	if (t == generic2_def) return yellow "generic2_def" reset;
+
+	if (t == label_def) return magenta "label_def" reset;
+	if (t == label2_def) return magenta "label2_def" reset;
+
+	if (t == forward_def) return red "forwards_def" reset;
+	if (t == backward_def) return green "backward_def" reset;
+
+	if (t == var_def) return cyan "var_def" reset;
+	return "unknown";
+}
+
+static const char* spell_ins(nat t) { 
+	if (t == nulli) return "{null}";
+	if (t == nop) return magenta "nop" reset;
+	if (t == add) return red "add" reset;
+	if (t == _xor) return green "xor" reset;
+	if (t == bne) return blue "bne" reset;
+	return "unknown";
+}
+
+static void print_name(struct word w) {
+	for (nat i = 0; i < w.length; i++) putchar(w.name[i]);
+}
+
+static void print_word(struct word w) {
+	print_name(w);
+	printf(reset "  :  {  .type = %s, .value = %lu } \n", 
+		spell_type(w.type), 
+		w.value
+	);
+}
+
+static void print_instruction(nat index) {
+
+	printf("[ ");
+	print_name(dictionary[instructions[4 * index + 1]]); printf("(%s) = ", spell_type(dictionary[instructions[4 * index + 1]].type));
+	printf("%s { ", spell_ins(instructions[4 * index + 0]));
+	print_name(dictionary[instructions[4 * index + 2]]); printf("(%s), ", spell_type(dictionary[instructions[4 * index + 2]].type));
+	print_name(dictionary[instructions[4 * index + 3]]); printf("(%s) }",  spell_type(dictionary[instructions[4 * index + 3]].type));
+
+ 	printf(" ]\t\t{%lu %lu %lu %lu}\n",
+		instructions[4 * index + 0], 
+		instructions[4 * index + 1], 
+		instructions[4 * index + 2], 
+		instructions[4 * index + 3]
+	);
+}
+
+static void print_arguments() {
+	printf("arguments { \n");
+	for (nat i = 0; i < 32; i++) {
+		printf("\t%3lu : %lu \n", i, arguments[i]);
+	}
+	printf("}\n");
+}
+
+static void print_dictionary() {
+	printf("dictionary { \n");
+	for (nat i = 0; i < dictionary_count; i++) {
+		printf("\t%3lu  :  " green, i);
+		print_word(dictionary[i]);
+	}
+	printf("}\n");
+}
+
+static void print_instructions() {
+	printf("instructions { \n");
+	for (nat i = 0; i < ins_count; i++) {
+		printf("\t%3lu  :  " , i);
+		print_instruction(i);
+	}
+	printf("}\n");
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+
+static void ins(nat op) {
+	instructions = realloc(instructions, sizeof(nat) * 4 * (ins_count + 1));
+	instructions[4 * ins_count + 0] = op;
+	instructions[4 * ins_count + 1] = arguments[0];
+	instructions[4 * ins_count + 2] = arguments[1];
+	instructions[4 * ins_count++ + 3] = arguments[2];
+}
+
+static void branch_check() {
+	struct word* label = dictionary + instructions[4 * (ins_count - 1) + 1];
+
+	     if (label->type == generic_def or label->type == label_def)   label->type = forward_def;
+	else if (label->type == generic2_def or label->type == label2_def) label->type = backward_def;
+
+	else if (label->type == var_def) {
+		printf(red "error: found variable instead of label in branch: " reset);
+		print_instruction(ins_count - 1);
+	}
+
+	if (debug) printf(cyan "info: generating branch with: " reset);
+	if (debug) print_instruction(ins_count - 1);
+}
+
+static void operation_check() {
+	for (nat i = 1; i < 4; i++) {
+		struct word* argument = dictionary + instructions[4 * (ins_count - 1) + i];
+
+		if (	argument->type == forward_def or argument->type == backward_def or
+			argument->type == label_def or argument->type == label2_def) {
+
+			if (argument->type == label2_def) argument->type = label_def;
+
+			printf(red "error: found label instead of variable at argument %lu in operation: " reset, i);
+			print_instruction(ins_count - 1);
+
+		} else argument->type = var_def;
+	}
+	if (debug) printf(cyan "info: generating operation with: " reset);
+	if (debug) print_instruction(ins_count - 1);
+}
+
+static void push_argument(nat argument) {
+	for (nat a = 31; a; a--) arguments[a] = arguments[a - 1]; 
+	arguments[0] = argument;
 }
 
 static void interpret(char* string, nat length) {
-	
+
 	nat count = 0, start = 0;
 	for (nat i = 0; i < length; i++) {
+
 		if (not isspace(string[i])) { 
 			if (not count) start = i; 
 			count++; continue; 
 		} else if (not count) continue;
+
 		process_word:; 
 		char* word = string + start;
 
-		     if (not strncmp(word, "nop", count)) ins(nop,0,0,0);
-		else if (not strncmp(word, "add", count)) ins(add,0,0,0);
-		else if (not strncmp(word, "bne", count)) ins(bne,0,0,0);
+		     if (not strncmp(word, "nop", count)) ins(nop);
+		else if (not strncmp(word, "add", count)) { ins(add); operation_check(); } 
+		else if (not strncmp(word, "xor", count)) { ins(_xor); operation_check(); } 
+		else if (not strncmp(word, "bne", count)) { ins(bne); branch_check(); }
 
 		else {
-			printf("found label: "); 
-			for (nat _ = 0; _ < count; _++) putchar(string[_ + start]);
-			puts("");
+			for (nat d = 0; d < dictionary_count; d++) {
+				if (strncmp(dictionary[d].name, word, count)) continue;
 
-			for (nat entry = 0; entry < dictionary_count; entry++) {
-				if (strncmp(dictionary[entry].name, word, count)) continue;
-				printf("found existing label!\n");
-				goto found;
+				if (dictionary[d].type == forward_def)      dictionary[d].type = label_def;
+				else if (dictionary[d].type == generic_def) dictionary[d].type = generic2_def;
+				else if (dictionary[d].type == label_def)   dictionary[d].type = label2_def;
+
+				if (debug) printf("[DEFINED]    ");
+				if (debug) print_word(dictionary[d]);
+
+				push_argument(d);
+				goto finish_word;
 			}
 
-			printf("making a new label (Adding to dict!)....\n");
+			push_argument(dictionary_count);
 			dictionary = realloc(dictionary, sizeof(struct word) * (dictionary_count + 1));
-			dictionary[dictionary_count++] = (struct word) {.name = strndup(word, count), .length = count, .type = label_definition};
-			found:;
+			dictionary[dictionary_count++] = (struct word) {.name = strndup(word, count), .length = count, .type = generic_def, .value = 0};
+
+			if (debug) printf("[not defined]  -->  assuming  ");
+			if (debug) print_word(dictionary[dictionary_count - 1]);
+
+			finish_word:
+			if (dictionary[*arguments].type == generic_def or dictionary[*arguments].type == label_def) 
+				dictionary[*arguments].value = ins_count;
 		}
 		count = 0;
 	}
@@ -95,6 +220,9 @@ loop: 	printf(" : ");
 	line[--length] = 0;
 	if (not strcmp(line, "q")) goto done;
 	else if (not strcmp(line, "o")) printf("\033[H\033[J");
+	else if (not strcmp(line, "arguments")) print_arguments();
+	else if (not strcmp(line, "dictionary")) print_dictionary();
+	else if (not strcmp(line, "instructions")) print_instructions();
 	else interpret(line, length);
 	goto loop; done:;
 }
@@ -102,13 +230,26 @@ loop: 	printf(" : ");
 
 
 
+/*
+
+	zero zero zero xor
+
+	zero one label bne
+
+	label
+
+	zero label pasta add
+
+	nop
+
+	nop
+
+	one one label bne
 
 
 
 
-
-
-
+*/
 
 
 
@@ -129,6 +270,33 @@ loop: 	printf(" : ");
 
 
 /*
+
+label 
+zero one label bne
+
+
+zero one label bne
+
+
+
+
+
+
+
+
+
+
+
+else if (not strcmp(line, "i")) interpret(strdup(test_string), strlen(test_string));
+
+
+
+
+
+
+
+
+
 
 printf("%c [%lu] ", string[i], count);
 puts("");
@@ -195,5 +363,105 @@ static char* read_file(const char* filename, size_t* out_length) {
 
 
 
+static nat data_node_count = 0;
+static struct data_node data_nodes = NULL;
+
+
+
+
+//struct data_node { nat type, value; };
+
+
+data_node = realloc(data_node, sizeof(struct data_node) * (data_node_count + 1));
+			data_node[data_node_count++] = (struct data_node) {...};
+
+
+
+// if we see that the label argument's data node   has a value which is 0, then we know it was not defined yet. overwrite its type with 
+
+			// if (not data_node[*arguments].value) data_node[*arguments].type = forward_label;
+
+
+
+if (data_node[dictionary[entry].index].type == forward_label) {
+					
+				}
+
+
+
+  // then define the name as a variable, with the data_node of the expression!
+
+
+// define the name as a label. 
+
+
+
+
+
+if (data_nodes[*arguments].type == expression) {  
+				
+			} else {  
+				
+			}
+
+
+
+
+enum data_node_type { nulld, variable, label, forward_label };
+
+
+
+if (dictionary[instructions[4 * (ins_count - 1) + 1]].use_count == 1) {
+
+			if (debug) printf(red "ERROR: forward declaring label \"");
+			if (debug) print_word(dictionary[instructions[4 * (ins_count - 1) + 1]]);
+
+			type = 
+			
+		} else {
+			if (debug) printf(cyan "info: found already declared label.\n" reset);
+			if (debug) printf("[.use_count = %lu]\n", dictionary[instructions[4 * (ins_count - 1) + 1]].use_count);
+		}
+
+
+if (debug) printf(red "ERROR: type mismatch. expected label variable for branch arg0. ...aborting...\n" reset);
+		if (debug) printf("[.type = %lu]\n", dictionary[instructions[4 * (ins_count - 1) + 1]].type);
+		if (debug) abort();
+
+
+
+/// if (dictionary[d].type == label_def) dictionary[d].type = backward_def;
+
+
+
+
+if (dictionary[instructions[4 * (ins_count - 1) + 1]].type == forward_def) {
+		printf("forward def\n");
+		
+	} else {
+		printf("label def\n");
+	}
+
+
+
+ puts(red "FORWARDS BRANCH" reset);
+
+
+ puts(green "BACKWARDS BRANCH" reset);
+
+  puts(red "DOUBLE FORWARDS BRANCH" reset);  
+
+puts(red "IMM FORWARDS BRANCH" reset);
+
+
+
+
+	//else if (label->type == forward_def) {}  
+	//else if (label->type == backward_def) {} 
+	//else 
+
 */
+
+
+
 
