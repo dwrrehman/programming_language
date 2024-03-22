13cfb80fd17df8b7365c0ecf57af35049d9a1284
Daniel Rehman <dwrrehman@gmail.com>
2024-03-22 03:48:38 -0700

    add the new frontend string parser thingy with no comments in it anymore

--- string_based_language_test/c.c
+++ string_based_language_test/c.c
@@ -12,10 +12,23 @@
 
 typedef uint64_t nat;
 
-static bool equals(const char* literal, char* text, nat start) {
-	nat i = start, j = 0;
+static char* text = NULL;
+static nat text_length = 0;
+
+static nat registers[4096] = {0}; 
+static nat arguments[4096] = {0};
+
+static nat names[4096] = {0};
+static nat lengths[4096] = {0};
+static nat values[4096] = {0};
+
+static nat name_count = 0, length = 0, 
+	start = 0, argument_count = 0, 
+	sn = 0;
+
+static bool equals(const char* literal, nat initial) {
+	nat i = initial, j = 0;
 	for (; text[i] != '"' and literal[j]; i++) {
-		//printf("%c", text[i]);
 		if ((unsigned char) text[i] < 33) continue;
 		if (text[i] != literal[j]) return false;
 		j++;
@@ -23,136 +36,153 @@ static bool equals(const char* literal, char* text, nat start) {
 	return text[i] == '"' and not literal[j];
 }
 
+static void process(nat n, nat name) {
+
+	if (equals("snincr", n)) { sn++; }
+	else if (equals("snzero", n)) { sn = 0; } 
+	else if (equals("snshl", n)) { sn <<= 1; } 
+	else if (equals("0", n)) { sn <<= 1; } 
+	else if (equals("1", n)) { sn++; sn <<= 1; } 
+	else if (equals("#", n)) { arguments[argument_count++] = sn; } 
+	else if (equals("snpush", n)) { arguments[argument_count++] = sn; } 
+	else if (equals("snprint", n)) { printf("sn = %llu\n", sn); } 
+	else if (equals("argprint", n)) { printf("debug: %llu\n", arguments[argument_count - 1]); } 
+	else if (equals("debugarguments", n)) { 
+		printf("debug: .args={ ");
+		for (nat a = 0; a < argument_count; a++) printf("%llu ", arguments[a]);
+		puts("}");
+	} else arguments[argument_count++] = values[name];
+
+}
+
 int main(int argc, const char** argv) {
-	if (argc == 1) exit(puts("usage error, please give a file."));
+
+	if (argc < 2) exit(puts("usage: ./assembler <source.s>"));
+
 	int file = open(argv[1], O_RDONLY);
 	if (file < 0) { perror("open"); exit(1); } 
 	struct stat st;
 	fstat (file, &st);
-	const nat text_length = (nat) st.st_size;
-	char* text = mmap(0, text_length, PROT_READ, MAP_PRIVATE, file, 0);
+	text_length = (nat) st.st_size;
+	text = mmap(0, text_length, PROT_READ, MAP_PRIVATE, file, 0);
 	close(file);
 
-
-	nat sn = 0;
-	nat arguments[4096] = {0};
-	nat names[4096] = {0};
-	nat lengths[4096] = {0};
-	nat values[4096] = {0};
-	nat name_count = 0, length = 0, start = 0, argument_count = 0;
-
-	puts("parsing file now...");
-
-	//puts("------------------------");
 	for (nat index = 0; index < text_length; index++) {
-
-		if ((unsigned char) text[index] < 33) printf("\033[7;33m");
-		//printf("%c", text[index]);
-		if ((unsigned char) text[index] < 33) printf("\033[0m");
-		if ((unsigned char) text[index] < 33) continue;
-
-		if (start) { length++; } 
-
+		if ((unsigned char) text[index] < 33) goto next_char;
 		if (text[index] == '"') {
-			if (not start) { start = index + 1; length = 0; } 
-			else {
-				nat spot = name_count;
-				for (nat i = 0; i < name_count; i++) {
-					
-					if (length >= lengths[i]) { spot = i; break; } //  puts("FOUND"); 
-					//puts("LOOKED");
-				}
-
-				//printf("[start=%llu,length=%llu,spot=%llu]", start, length, spot);
+			if (not start) { 
+				start = index + 1; 
+				length = 0; 
+			} else {
+				nat spot = 0;
+				for (; spot < name_count; spot++) if (length >= lengths[spot]) break;
 				memmove(lengths + spot + 1, lengths + spot, sizeof(nat) * (name_count - spot));
 				memmove(names + spot + 1, names + spot, sizeof(nat) * (name_count - spot));
 				memmove(values + spot + 1, values + spot, sizeof(nat) * (name_count - spot));
-
-				lengths[spot] = length - 1;
+				lengths[spot] = length;
 				names[spot] = start;
 				values[spot] = argument_count ? arguments[argument_count - 1] : 0;
-
 				name_count++; start = 0;
 			}
-			//if (start) printf("\033[32m"); else printf("\033[0m");
-			continue;
+			goto next_char;
 		}
-
-		if (start) continue;
-
-		for (nat j = 0; j < name_count; j++) {
-			//printf("@%llu:%llu ", names[j], lengths[j]);
-			nat c = names[j], i = 0;
+		if (start) { length++; goto next_char; } 
+		nat imax = 0;
+		for (nat name = 0; name < name_count; name++) {
+			nat c = names[name], n = c, i = 0;
 			for (; text[c] != '"'; ) {
-				//printf("\n\tchecking @(index=%llu)\"%c\" vs @(name=%llu)\"%c\"...\n", 
-				//	index, text[index + i], c, text[c]);				
-
+				if (i > imax) imax = i;
+				if (index + i >= text_length) goto next_name;
 				if ((unsigned char) text[index + i] < 33) { i++; continue; }
 				if ((unsigned char) text[c] < 33) { c++; continue; }
 				if (text[index + i] != text[c]) goto next_name;
 				i++; c++;
 			}
-			printf("[found the name: <<<");
-			for (nat cc = names[j]; text[cc] != '"'; cc++) putchar(text[cc]);
-			
-			printf(">>>, ");
-			index += i;
-			printf("moved along %llu chars...\n", i);
-			
-			     if (equals("snincr",  text, names[j])) { sn++; }
-			else if (equals("snzero",  text, names[j])) { sn = 0; } 
-			else if (equals("snshl",   text, names[j])) { sn <<= 1; } 
-			else if (equals("0",   text, names[j])) { sn <<= 1; } 
-			else if (equals("1",   text, names[j])) { sn++; sn <<= 1; } 
-			else if (equals("#",   text, names[j])) { arguments[argument_count++] = sn; } 
-			else if (equals("snpush",  text, names[j])) { arguments[argument_count++] = sn; } 
-			else if (equals("snprint", text, names[j])) { printf("sn = %llu\n", sn); } 
-			else if (equals("argprint", text, names[j])) { printf("debug: %llu\n", arguments[argument_count - 1]); } 
-			else if (equals("debugarguments", text, names[j])) { 
-
-				printf("debug: .args={ ");
-				for (nat a = 0; a < argument_count; a++) {
-					printf("%llu ", arguments[a]);
-				}
-				puts("}");
-
-			} else { 
-				arguments[argument_count++] = values[j];
-			}
-
-			goto found_name;
+			process(n, name);
+			index += i - 1;
+			goto next_char;
 			next_name:;
 		}
-		printf("error: could not resolve symbol near %llu, aborting.\n", index);
-		abort();
-		found_name:;
-		
+		printf("asm: \033[31;1merror:\033[0m %s:%llu:%llu: unresolved symbol\n", 
+			argv[1], index, index + imax
+		);
+		exit(1);
+		next_char:;
 	}
-	//puts("------------------------");
 
-	puts("current names:");
+
+
+	puts("current names: {");
 	for (nat i = 0; i < name_count; i++) {
-		printf("#%llu: @%llu:len=%llu:val=%llu ", i, names[i], lengths[i], values[i]);
+		printf("\t#%llu: @%llu:len=%llu:val=%llu ", i, names[i], lengths[i], values[i]);
 		for (nat c = names[i]; text[c] != '"'; c++) putchar(text[c]);
 		puts("");
 	}
+	puts("}");
 
 	munmap(text, length);
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 
+//printf("[success]\n");
+//printf("\n\tchecking name[%llu]=%llu::: @(index=%llu)\"%c\" vs @(name=%llu)\"%c\"...\n", j, names[j], index, text[index + i], c, text[c]);
 
+
+
+	//puts("parsing file now...");
+
+	//puts("------------------------");
+
+//if ((unsigned char) text[index] < 33) printf("\033[7;33m");
+		//printf("%c", text[index]);
+		//if ((unsigned char) text[index] < 33) printf("\033[0m");
+
+ //  puts("FOUND"); 
+//puts("LOOKED");
+
+//printf("[start=%llu,length=%llu,spot=%llu]", start, length, spot);
+
+
+
+//if (start) printf("\033[32m"); else printf("\033[0m");
 //puts("toggling name state!");
 			//puts(defining ? "[defining]" : "[executing]");
 
 
+//printf("@%llu:%llu ", names[j], lengths[j]);
+//printf("\n\tchecking @(index=%llu)\"%c\" vs @(name=%llu)\"%c\"...\n", 
+				//	index, text[index + i], c, text[c]);				
 
+//printf("[found the name: <<<");
+			//for (nat cc = names[j]; text[cc] != '"'; cc++) putchar(text[cc]);
+			
+			//printf(">>>, ");
 
-
-
-
-
+//printf("moved along %llu chars...\n", i);
 
 */
 
